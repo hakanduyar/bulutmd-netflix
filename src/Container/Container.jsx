503245770e15829dcc0ff8f11516c01f2abfc4f2
rikords <79376651+rikords@users.noreply.github.com>
2023-02-16 18:19:20 +0300

    Added Component, Container, Css and Pages folders.

---
+++ src/Container/Container.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "../Component/Navbar/Navbar";
+
+function Container() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default Container;
