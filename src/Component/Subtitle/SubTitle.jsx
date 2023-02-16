b911fbf4644bc9f56eace96316a8d5d014ae17a2
rikords <79376651+rikords@users.noreply.github.com>
2023-02-16 20:59:34 +0300

    Added subtitles css and container files

---
+++ src/Component/Subtitle/SubTitle.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SubTitle() {
+  return (
+    <div className="sub-title ">
+      <h3>Popüler Başlıklar</h3>
+    </div>
+  );
+}
+
+export default SubTitle;
--- src/Container/Container.jsx
+++ src/Container/Container.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "../Component/Navbar/Navbar";
+import SubTitle from "../Component/Subtitle/SubTitle";
+import "../Css/Container/Container.css";
+import "../Css/SubTitle/Subtitle.css";
 
 function Container() {
   return (
     <div>
       <Navbar />
+      <div className="container">
+        <SubTitle />
+      </div>
     </div>
   );
 }
